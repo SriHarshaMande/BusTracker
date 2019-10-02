5a539da74be1974e8a2bb02f4f7d0b266373a2f9
hac <mandesriharsha@gmail.com>
2019-10-02 15:05:16 +0530

    firebase Auth check

--- app/src/main/java/com/vnr/bustracker/MainActivity.java
+++ app/src/main/java/com/vnr/bustracker/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
 
+public class MainActivity extends AppCompatActivity
+{
+    FirebaseAuth mauth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(getApplicationContext());
+        mauth=FirebaseAuth.getInstance();
     }
 }
